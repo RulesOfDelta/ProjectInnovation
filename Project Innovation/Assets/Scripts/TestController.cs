54321973b93b97fc30ee516dc3aca9e43ac04cff
Tobias Nienhaus <tobiasnienhaus69@gmail.com>
2020-11-18 16:26:27 +0100

    Add basic InputHandler and TestController

---
+++ Project Innovation/Assets/Scripts/TestController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestController : MonoBehaviour
+{
+    [SerializeField] private InputHandler handler;
+
+    [SerializeField] private float rotSpeed = 10f;
+    [SerializeField] private float moveSpeed = 10f;
+
+    private void Start()
+    {
+        handler.RegisterOnFire(OnFire);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, Time.deltaTime * handler.State.look.x * rotSpeed, 0);
+        var move = handler.State.move * (Time.deltaTime * moveSpeed);
+        transform.Translate(move.x, 0, move.y);
+    }
+
+    private void OnFire()
+    {
+        
+    }
+}
---
+++ Project Innovation/Assets/Scripts/input/InputHandler.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+using Vector2 = UnityEngine.Vector2;
+
+public class InputHandler : MonoBehaviour
+{
+    public class InputState
+    {
+        public Vector2 move;
+        public Vector2 look;
+    }
+
+    public delegate void ButtonCallback();
+
+    private event ButtonCallback OnFireCb;
+    
+    public InputState State { get; private set; }
+
+    private void Start()
+    {
+        State = new InputState();
+    }
+
+    public void RegisterOnFire(ButtonCallback cb)
+    {
+        OnFireCb += cb;
+    }
+
+    public void DeregisterOnFire(ButtonCallback cb)
+    {
+        OnFireCb -= cb;
+    }
+
+    public void OnFire(InputAction.CallbackContext context)
+    {
+        if (context.ReadValueAsButton() && context.action.triggered)
+        {
+            OnFireCb?.Invoke();
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        State.move = context.ReadValue<Vector2>();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        State.look = context.ReadValue<Vector2>();
+    }
+}

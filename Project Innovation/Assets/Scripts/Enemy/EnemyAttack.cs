07d57d6c4be0bffac2992b6e7d48c119820183e3
Tobias Nienhaus <tobiasnienhaus69@gmail.com>
2020-11-19 16:27:35 +0100

    Let player take damage

---
+++ Project Innovation/Assets/Scripts/Enemy/EnemyAttack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    [SerializeField] private PlayerStats playerStats;
+    
+    [SerializeField] private float attackDist = 1f;
+    [SerializeField] private float attackTime = 1f;
+    [SerializeField] private float damage;
+
+    private void Start()
+    {
+        StartCoroutine(AttackLoop());
+    }
+
+    private IEnumerator AttackLoop()
+    {
+        while (gameObject)
+        {
+            if (Vector3.Distance(playerStats.transform.position, transform.position) < attackDist)
+            {
+                playerStats.Health -= damage;
+                Debug.Log("Attack player");
+                yield return new WaitForSeconds(attackTime);
+            }
+            else
+                yield return null;
+        }
+    }
+}

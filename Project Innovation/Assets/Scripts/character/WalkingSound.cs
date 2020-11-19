ff7867cf461a26446737bcade998928806a36102
Maurice Johannssen <60900273+RulesOfDelta@users.noreply.github.com>
2020-11-19 14:07:58 +0100

    Add footstep scripts

---
+++ Project Innovation/Assets/Scripts/character/WalkingSound.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class WalkingSound : MonoBehaviour
+{
+    private float _distanceTraveled;
+    private Vector3 _oldPosition;
+    public float stepDistance = 1.2f;
+
+    [FMODUnity.EventRef]
+    public string fmodEventPath;
+
+    void Start()
+    {
+        _distanceTraveled = 0;
+        _oldPosition = transform.position;
+    }
+    
+    void Update()
+    {
+        _distanceTraveled += (transform.position - _oldPosition).magnitude;
+        if(_distanceTraveled >= stepDistance + Random.Range(0, 0.5f))
+        {
+            PlayFootstepSound();
+            _distanceTraveled = 0;
+        }
+    }
+
+    private void PlayFootstepSound()
+    {
+        FMOD.Studio.EventInstance eventInstance = FMODUnity.RuntimeManager.CreateInstance(fmodEventPath);
+        eventInstance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(transform.position));
+
+        eventInstance.start();
+        eventInstance.release();
+    }
+}

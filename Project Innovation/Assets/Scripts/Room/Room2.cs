f82ae67c2e0c7e292624277f9424cef07b6c5d47
Tobias Nienhaus <tobiasnienhaus69@gmail.com>
2020-11-24 10:37:25 +0100

    Improve generation

--- Project Innovation/Assets/Scripts/Room/Room2.cs
+++ Project Innovation/Assets/Scripts/Room/Room2.cs
@@ -12,9 +12,15 @@ public class Room2 : MonoBehaviour
     [SerializeField] private float maxSize = 50f;
     [SerializeField] private float wallHeight = 10f;
 
+    [SerializeField] private float doorWidth = 2f;
+    [SerializeField, Range(0f, 1f)] private float doorSpace = 0.5f;
+
     [SerializeField] private Transform wallPrefab;
     [SerializeField] private Transform floor;
     private List<Transform> walls;
+
+    [SerializeField, Min(0)] private int minDoorCount;
+    [SerializeField, Min(0)] private int maxDoorCount;
 
     // Start is called before the first frame update
     void Start()
@@ -31,7 +37,7 @@ public class Room2 : MonoBehaviour
         }
     }
 
-    public struct Wall
+    private readonly struct Wall
     {
         // TODO naming
         public readonly float Distance;
@@ -40,7 +46,7 @@ public class Room2 : MonoBehaviour
         public readonly float Size;
         public readonly bool Horizontal;
 
-        public Wall(float distance, float axisPos, Quaternion rotation, float size, bool horizontal)
+        private Wall(float distance, float axisPos, Quaternion rotation, float size, bool horizontal)
         {
             Distance = distance;
             AxisPos = axisPos;
@@ -64,7 +70,27 @@ public class Room2 : MonoBehaviour
         }
     }
 
+    private bool inEditMode;
+
     [ContextMenu("Generate")]
+    private void EditModeGenerate()
+    {
+        if (!inEditMode)
+        {
+            EditorApplication.playModeStateChanged += EditModeHandleStateChange;
+            inEditMode = true;
+        }
+        Generate();
+    }
+
+    private void EditModeHandleStateChange(PlayModeStateChange newState)
+    {
+        if (newState != PlayModeStateChange.ExitingEditMode) return;
+        EditorApplication.playModeStateChanged -= EditModeHandleStateChange;
+        Clear();
+        inEditMode = false;
+    }
+
     private void Generate()
     {
         Clear();
@@ -74,21 +100,16 @@ public class Room2 : MonoBehaviour
 
         var preWalls = new List<Wall>
         {
-            new Wall(-y / 2f, Quaternion.Euler(0, 0, 0), x, false)
+            new Wall(-y / 2f, Quaternion.Euler(0, 0, 0), x, false),
+            new Wall(-x / 2f, Quaternion.Euler(0, 90, 0), y, true),
+            new Wall(y / 2f, Quaternion.Euler(0, 180, 0), x, false),
+            new Wall(x / 2f, Quaternion.Euler(0, 270, 0), y, true)
         };
-        SplitWallAtEnd(preWalls);
 
-        preWalls.Add(new Wall(-x / 2f, Quaternion.Euler(0, 90, 0), y, true));
-        SplitWallAtEnd(preWalls);
-
-        preWalls.Add(new Wall(y / 2f, Quaternion.Euler(0, 180, 0), x, false));
-        SplitWallAtEnd(preWalls);
-
-        preWalls.Add(new Wall(x / 2f, Quaternion.Euler(0, 270, 0), y, true));
-        SplitWallAtEnd(preWalls);
+        var splitCount = Random.Range(minDoorCount, maxDoorCount);
+        for (var i = 0; i < splitCount; i++) SplitWallInList(preWalls, Random.Range(0, preWalls.Count - 1));
 
         FillFromWalls(preWalls);
-
         // TODO no magic division by 10
         floor.localScale = new Vector3(x / 10, 1, y / 10);
     }
@@ -102,6 +123,11 @@ public class Room2 : MonoBehaviour
     {
         var wall = wallList[index];
         wallList.RemoveAt(index);
+        SplitWallAndAdd(wallList, wall);
+    }
+
+    private void SplitWallAndAdd(ICollection<Wall> wallList, Wall wall)
+    {
         SplitWall(wall).Deconstruct(out var wall1, out var wall2);
         wallList.Add(wall1);
         wallList.Add(wall2);
@@ -111,13 +137,15 @@ public class Room2 : MonoBehaviour
     private Tuple<Wall, Wall> SplitWall(Wall wall)
     {
         // TODO parameters for door width and empty room at each side
-        var maxDist = (wall.Size / 2f - 1f) * 0.8f;
+        var maxDist = (wall.Size / 2f - doorWidth) * doorSpace;
         var split = Random.Range(-maxDist, maxDist);
         // TODO leave room for door
-        var sizeA = wall.Size / 2f - split;
-        var sizeB = wall.Size / 2f + split;
-        var splitPosA = wall.AxisPos - split - sizeA / 2f - 0.5f;
-        var splitPosB = wall.AxisPos - split + sizeB / 2f + 0.5f;
+        var sizeA = wall.Size / 2f - split - doorWidth / 2f;
+        sizeA = Mathf.Max(0f, sizeA);
+        var sizeB = wall.Size / 2f + split - doorWidth / 2f;
+        sizeB = Mathf.Max(0f, sizeB);
+        var splitPosA = wall.AxisPos - split - sizeA / 2f - doorWidth / 2f;
+        var splitPosB = wall.AxisPos - split + sizeB / 2f + doorWidth / 2f;
 
         return new Tuple<Wall, Wall>(wall.Split(splitPosA, sizeA),
             wall.Split(splitPosB, sizeB));
@@ -149,16 +177,25 @@ public class Room2 : MonoBehaviour
         if (EditorApplication.isPlaying)
         {
             foreach (var w in walls)
-                Destroy(w.gameObject);
+            {
+                if(w)
+                    Destroy(w.gameObject);
+            }
         }
         else
         {
             foreach (var w in walls)
-                DestroyImmediate(w.gameObject);
+            {
+                if(w)
+                    DestroyImmediate(w.gameObject);
+            }
         }
 #else
-        foreach (var w in walls)
-            Destroy(w.gameObject);
+        foreach (var w in walls) 
+        {
+            if(w)
+                Destroy(w.gameObject);
+        }
 #endif
         walls.Clear();
     }

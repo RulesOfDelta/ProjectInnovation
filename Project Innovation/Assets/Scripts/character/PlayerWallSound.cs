9b68c3f704615af9068f1ae5bc7081f61744da20
Tobias Nienhaus <tobiasnienhaus69@gmail.com>
2020-11-30 14:58:57 +0100

    Finish wall sound

--- Project Innovation/Assets/Scripts/character/PlayerWallSound.cs
+++ Project Innovation/Assets/Scripts/character/PlayerWallSound.cs
@@ -9,6 +9,8 @@ public class PlayerWallSound : MonoBehaviour
     [FMODUnity.EventRef, SerializeField] private string wallHitEvent;
     [SerializeField] private InputHandler inputHandler;
 
+    [FMODUnity.EventRef, SerializeField] private string hummingSound;
+    private FMOD.Studio.EventInstance hummingInstance;
     [SerializeField] private float minDistance;
     [SerializeField] private float maxDistance;
     [SerializeField] private AnimationCurve falloff = AnimationCurve.EaseInOut(0f, 1f, 1f, 0f);
@@ -16,21 +18,34 @@ public class PlayerWallSound : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        hummingInstance = hummingSound.CreateSound();
+        hummingInstance.setParameterByName("HumVolume", 0f);
+        hummingInstance.start();
+    }
+
+    private void OnDestroy()
+    {
+        hummingInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        hummingInstance.release();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float val;
         // TODO do raycasts to walls
-        if (Physics.Raycast(transform.position, transform.forward, 
+        if (Physics.Raycast(transform.position, transform.forward,
             out var hit, maxDistance, playerWallMask))
         {
-            var val = hit.distance < minDistance
-                ? 1f 
-                : falloff.Evaluate((hit.distance - minDistance) / (maxDistance - maxDistance));
-            inputHandler.Rumble(val, val);
+            val = hit.distance < minDistance
+                ? 1f
+                : falloff.Evaluate((hit.distance - minDistance) / (maxDistance - minDistance));
         }
+        else
+            val = 0f;
+
+        hummingInstance.setParameterByName("HumVolume", val);
+        inputHandler.Rumble(val, val);
     }
 
     private void OnCollisionEnter(Collision other)
